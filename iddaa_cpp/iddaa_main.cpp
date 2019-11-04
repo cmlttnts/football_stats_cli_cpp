f23d1fe7016fbcbe7d5971fdde5c0c023bd3e05a
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2019-11-04 07:30:04 +0300

    main refactor whiles swtiches organized

--- iddaa_cpp/iddaa_main.cpp
+++ iddaa_cpp/iddaa_main.cpp
@@ -15,7 +15,7 @@
 
 
 
-int main(){
+int main() {
 	std::string exit_input = "q";
 	std::string user_input;
 	std::cout << "Press 'q' for exit" << std::endl;
@@ -38,52 +38,153 @@ int main(){
 	bool go_back_to_team = false;
 	bool go_back_to_league = false;
 	while (!exited) {
-		INIT_OPTIONS first_option = getInitOptions();
-		go_back_to_init = false;
-		switch (first_option)
-		{
-		case INIT_OPTIONS::TEAM_OPTION:
-			while (!exited && !go_back_to_init) {
-				TEAM_OPTIONS second_option = getTeamOption();
-				go_back_to_team = false;
-				switch (second_option)
-				{
-				case TEAM_OPTIONS::TEAM_ANALYSIS:
-					while (!exited && !go_back_to_team) {
-						std::string team_name = getTeamNameFromUser();
-						int choice = presentTeam(team_name, match_file_names);
-						exited = choice == 1;
-						go_back_to_team = choice == 2;
+		INIT_OPTIONS first_opt = getInitOptions();
+		switch (first_opt) {
+			case INIT_OPTIONS::TEAM_OPTION:
+				go_back_to_init = false;
+				while (!exited && go_back_to_init) {
+					TEAM_OPTIONS team_opt = getTeamOption();
+					switch (team_opt) {
+						case TEAM_OPTIONS::TEAM_ANALYSIS:
+							go_back_to_team = false;
+							while (!exited && go_back_to_team) {
+								std::string team_name = getTeamNameFromUser();
+								//TODO
+							}
+							break;
+						case TEAM_OPTIONS::BEST_TEAM:
+							go_back_to_team = false;
+							while (!exited && go_back_to_team) {
+								BEST_TEAM_OPTIONS best_t_opt = getBestTeamOption();
+								switch (best_t_opt) {
+									case BEST_TEAM_OPTIONS::BEST_RESULTS:
+										break;
+									case BEST_TEAM_OPTIONS::WORST_RESULTS:
+										break;
+									case BEST_TEAM_OPTIONS::MOST_GOALS_SCORED:
+										break;
+									case BEST_TEAM_OPTIONS::LEAST_GOALS_SCORED:
+										break;
+									case BEST_TEAM_OPTIONS::MOST_GOALS_RECEIVED:
+										break;
+									case BEST_TEAM_OPTIONS::LEAST_GOALS_RECEIVED:
+										break;
+									case BEST_TEAM_OPTIONS::COMEBAKCS:
+										break;
+									case BEST_TEAM_OPTIONS::CORNERS:
+										break;
+									case BEST_TEAM_OPTIONS::GO_BACK:
+										go_back_to_team = true;
+										break;
+									case BEST_TEAM_OPTIONS::EXIT:
+										exited = true;
+										break;
+								}
+							}
+							break;
+						case TEAM_OPTIONS::GO_BACK:
+							go_back_to_init = true;
+							break;
+						case TEAM_OPTIONS::EXIT:
+							exited = true;
 					}
-					break;
-				case TEAM_OPTIONS::BEST_TEAM:
-					getBestTeamOption();
-					//std::cout << "Cok Yakinda Geliyor...\n";
-					go_back_to_team = true;
-					break;
-				case TEAM_OPTIONS::GO_BACK:
-					go_back_to_init = true;
-					break;
-				case TEAM_OPTIONS::EXIT:
-					exited = true;
-					break;
-				default:
-					break;
 				}
-			}
-			break;
-		case INIT_OPTIONS::LEAGUE_OPTION:
-			std::cout << "Cok Yakinda Geliyor...\n";
-			go_back_to_init = true;
-			break;
-		case INIT_OPTIONS::TEAM_NAMES_OPTION:
-			printTeamsPerLeague(team_file_names);
-			break;
-		case INIT_OPTIONS::EXIT:
-			exited = true;
-		default:
-			break;
+				break;
+			case INIT_OPTIONS::LEAGUE_OPTION:
+				break;
+			case INIT_OPTIONS::TEAM_NAMES_OPTION:
+				break;
+			case INIT_OPTIONS::EXIT:
+				break;
 		}
 	}
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		INIT_OPTIONS first_option = getInitOptions();
+//		go_back_to_init = false;
+//		while (!exited && !go_back_to_init) {
+//			switch (first_option){
+//				case INIT_OPTIONS::TEAM_OPTION:
+//					TEAM_OPTIONS second_option = getTeamOption();
+//					go_back_to_team = false;
+//					while (!exited && !go_back_to_team) {
+//						switch (second_option){
+//							case TEAM_OPTIONS::TEAM_ANALYSIS:{
+//								std::string team_name = getTeamNameFromUser();
+//								int choice = presentTeam(team_name, match_file_names);
+//								exited = choice == 1;
+//								go_back_to_team = choice == 2;
+//							}
+//								break;
+//							case TEAM_OPTIONS::BEST_TEAM:
+//								while (!exited && !go_back_to_team) {
+//									BEST_TEAM_OPTIONS third_option = getBestTeamOption();
+//									switch (third_option){
+//										case BEST_TEAM_OPTIONS::BEST_RESULTS:
+//											break;
+//										case BEST_TEAM_OPTIONS::WORST_RESULTS:
+//											break;
+//										case BEST_TEAM_OPTIONS::MOST_GOALS_SCORED:
+//											break;
+//										case BEST_TEAM_OPTIONS::LEAST_GOALS_SCORED:
+//											break;
+//										case BEST_TEAM_OPTIONS::MOST_GOALS_RECEIVED:
+//											break;
+//										case BEST_TEAM_OPTIONS::LEAST_GOALS_RECEIVED:
+//											break;
+//										case BEST_TEAM_OPTIONS::COMEBAKCS:
+//											break;
+//										case BEST_TEAM_OPTIONS::CORNERS:
+//											break;
+//										case BEST_TEAM_OPTIONS::GO_BACK:
+//											go_back_to_team = true;
+//											break;
+//										case BEST_TEAM_OPTIONS::EXIT:
+//											exited = true;
+//											break;
+//										default:
+//											break;
+//									}
+//								}
+//								break;
+//							case TEAM_OPTIONS::GO_BACK:
+//								go_back_to_init = true;
+//								break;
+//							case TEAM_OPTIONS::EXIT:
+//								exited = true;
+//								break;
+//							default:
+//								break;
+//						}
+//					}
+//				break;
+//			case INIT_OPTIONS::LEAGUE_OPTION:
+//				std::cout << "Cok Yakinda Geliyor...\n";
+//				go_back_to_init = true;
+//				break;
+//			case INIT_OPTIONS::TEAM_NAMES_OPTION:
+//				printTeamsPerLeague(team_file_names);
+//				break;
+//			case INIT_OPTIONS::EXIT:
+//				exited = true;
+//			default:
+//				break;
+//			}
+//		}
+//	}
+//}
